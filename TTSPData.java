148dfacf127a07505d3437f263b7224d089391d5
mdeboute <martin.deboute@etu.u-bordeaux.fr>
2020-09-21 19:08:19 +0200

    mdeboute : add classes TTSPData and TTSPSolution

---
+++ TTSPData.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+final public class TTSPData{
+    private String name;
+    private Double domains;
+    private Double level;
+    private Double tech;
+    private Double interv;
+    private Double abandon;
+
+    private Double number_I;
+    private Double[] time;
+    private Double[] preds;
+    private Double[] prio;
+    private Double[] cost;
+    private Matrix r;
+
+    private Double number_T;
+    private Vector s;
+    private Double[] dispo;
+    
+}
---
+++ TTSPSolution.java
@@ -0,0 +1,11 @@
+final public class  TTSPSolution{
+    private double day;
+    private Matrix team_n;
+
+    private Matrix TT;
+
+    private double interv;
+    private double day_interv;
+    private double time;
+    private double team;
+}
