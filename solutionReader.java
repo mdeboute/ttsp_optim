c31e3cb4e30217226df7464fc320c5b0d08b441c
Gonzague Beutter <gonzague.bt@gmail.com>
2020-10-08 15:35:34 +0200

    ajout prémice code Reader

--- solutionReader.java
+++ solutionReader.java
@@ -1,3 +1,21 @@
+import java.io.*;
+
 public class solutionReader {
-    
+    public static void main(String[] args) throws IOException {
+      // Le fichier d'entrée
+      File file = new File("file.txt");
+      // Créer l'objet File Reader
+      FileReader fr = new FileReader(file);
+      // Créer l'objet BufferedReader  
+      BufferedReader br = new BufferedReader(fr);  
+      int c = 0;             
+      // Lire caractère par caractère
+      while((c = br.read()) != -1)
+      {
+            // convertir l'entier en char
+            char ch = (char) c;         
+            // Afficher le caractère      
+            System.out.println(ch);        
+      }
+   }
 }
