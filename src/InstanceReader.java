aeb96d2d4ab268d2d66ef0d2f600f7da8fc5158e
mdeboute <martin.deboute@etu.u-bordeaux.fr>
2020-10-14 18:44:55 +0200

    update somes files

--- src/InstanceReader.java
+++ src/InstanceReader.java
@@ -4,44 +4,16 @@ import java.io.*;
 import java.util.Scanner;
 
 public class InstanceReader {
-  private static int counter = 0;
-  private static TTSPData[] instance;
+  private static TTSPData ttspData;
 
-  public static void readFromFile(char file) throws FileNotFoundException {
-    Scanner scanner = new Scanner(new File(file));
-    try {
-      scanner.useDelimiter(" ");
-      String all = scanner.next();
-    } finally {
-        scanner.close();
+
+  public static void readFromFile(String file) throws FileNotFoundException {
+    try (Scanner scanner = new Scanner(new File(file))) {
+      System.out.println(scanner.nextLine());
     }
   }
-
-
-  public static void processLine(String line) {
-    Scanner scanner = new Scanner(line);
-    scanner.useDelimiter(" ");
-    if (scanner.hasNext()) {
-      instance[counter] = new TTSPData();
-      instance[counter].setName(scanner.next());
-      instance[counter].setDomains(scanner.next());
-      instance[counter].setLevel(scanner.next());
-      instance[counter].setTechs(scanner.next());
-      instance[counter].setInterv(scanner.next());
-      instance[counter].setAbandon(scanner.next());
-      counter++;
-    }
-    else {
-      System.out.println("Empty or invalid line. Unable to process.");
-    }
-  }
-
 
   public static void main(String[] args) throws FileNotFoundException {
-    instance = new TTSPData[6];
-    readFromFile(args[0]);
-    for(int i = 1; i < 6; i++) {
-      System.out.printf(instance[i].getName(), " ", instance[i].getDomains());
-    }
+    readFromFile("data/datasetA/data1/instance");
   }
 }
--- src/TTSPData.java
+++ src/TTSPData.java
@@ -1,11 +1,11 @@
 package src;
 
 public class TTSPData{
-    Instance instance;
-    IntervList interv_list;
-    TechList tech_list;
+    Instance[] instance;
+    IntervList[] interv_list;
+    TechList[] tech_list;
 
-    public TTSPData(Instance instance, IntervList interv_list, TechList tech_list) {
+    public TTSPData(Instance[] instance, IntervList[] interv_list, TechList[] tech_list) {
         this.instance=instance;
         this.interv_list=interv_list;
         this.tech_list=tech_list;
