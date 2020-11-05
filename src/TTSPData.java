e54de1d44e7accea27ba4c30b5f167ea878f7f02
gonzague.bt@gmail.com <gonzague.bt@gmail.com>
2020-11-05 17:20:21 +0100

    ajout commentaire

--- src/TTSPData.java
+++ src/TTSPData.java
@@ -4,8 +4,8 @@ import java.util.Arrays; // for the toString
 
 public class TTSPData {
     private final Instance instance;
-    private final IntervList[] intervention;
-    private final TechList[] technician;
+    private final IntervList[] intervention; //first IntervList[0] is an array of 0, interventions 1= IntervList[1],...
+    private final TechList[] technician; //first techList[0] is an array of 0, technician 1= TechList[1],...
 
     public TTSPData(Instance instance, IntervList[] intervention, TechList[] technician) {
         super();
