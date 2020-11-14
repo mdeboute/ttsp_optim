508b15a6ef825641f67d757a4bf10012cee2e0f1
gonzague.bt@gmail.com <gonzague.bt@gmail.com>
2020-11-14 20:16:21 +0100

    gbeutter : checker fonctionnel !

--- src/Checker.java
+++ src/Checker.java
@@ -2,6 +2,7 @@ package src;
 
 import src.dataClasses.*;
 import src.solClasses.*;
+import src.readers.*;
 
 public class Checker {
     public static int checker(TTSPData data, TTSPSolution solution) {
@@ -9,46 +10,39 @@ public class Checker {
         int nb_tech = data.getInstance().getTechs(); //number of technician
         int nb_interv = data.getInstance().getInterv(); // number of intervention
         int nb_domain = data.getInstance().getDomains(); //number of domains
-        int nb_level = data.getInstance().getLevel() - 1; //equal to the max level
-        int nb_days = solution.getInterv_dates().length; //lenght of tech-teams= number of days
+        int nb_level = data.getInstance().getLevel(); //equal to the max level
+        int nb_days = solution.getTech_teams().length; //length of tech-teams= number of days
 
         // Checks on technicians ///////////////////////////////////////////////////////////////
         for (int i = 1; i <= nb_tech; i++) { //browsing the list of technicians
-            for (int j = 1; j <= nb_days; j++) { // browsing the list of days
-
+            for (int j = 1; j < nb_days; j++) { // browsing the list of days or list of teams each days
                 //We check that the technician i unavailable on day j is assigned to team 0 /////////////////////
-                int check = 0;
-                for (int k = 1; k <= data.getTechnician()[i].getDispo().length; k++) { // browsing the list of days where technician i is unavailaible
-                    if (data.getTechnician()[i].getDispo()[k] == j) {
-                        check = i;
+                if(check(data.getTechnician()[i].getDispo(),j)){
+                    if(!check(solution.getTech_teams()[j].getTeam()[0],i)){
+                        System.out.print("[Issue] The technician " + i + " is assigned to a team the day " + j + " while he is not available this day.\n");
+                        feasible = 0;
+                        continue; // if the technician is not in the right team there is no point in making the following checks
                     }
-                }
-                for (int l = 1; l <= solution.getTech_teams()[j].getTeam().length; l++) {
-                    if (solution.getTech_teams()[j].getTeam()[0][l] == check) {
-                        check = -1;
-                    }
-                }
-                if (check != -1) {
-                    System.out.print("[Issue] The technician " + i + " is assigned to a team the day " + j + " while he is not available this day.\n");
-                    feasible = 0;
-                    break; // if the technician is not in the right team there is no point in making the following checks
                 }
 
                 // We check that technician i is assigned to a team on day j /////////////////////
                 int check2 = 0;
-                for (int l = 1; l < solution.getTech_teams()[j].getTeam().length; l++) { //browsing the list of team except team 0
-                    for (int k = 0; k < solution.getTech_teams()[j].getTeam()[l].length - 1; k++) { //browsing the list of technicians of eachteam
-                        if (solution.getTech_teams()[j].getTeam()[l].length == 0) {
+                for (int l = 0; l < solution.getTech_teams()[j].getTeam().length; l++) { //browsing the list of team except team 0
+                        if (solution.getTech_teams()[j].getTeam()[l]==null) {
                             System.out.print("[Issue] Team " + l + " is empty on day " + j + ".\n");
                             feasible = 0;
                         }
-                        if (solution.getTech_teams()[j].getTeam()[l][k] == i) { //
-                            check2 = -1;
+                        if (check(solution.getTech_teams()[j].getTeam()[l],i)){ //
+                            check2++;
                         }
-                    }
                 }
-                if (check2 != -1) {
+                if (check2==0) {
                     System.out.print("[Issue] Technician " + i + " is not assigned to any team on day " + j + ".\n");
+                    feasible = 0;
+                    //break; // if the technician is not assigned there is no point in carrying out the following checks
+                }
+                if (check2>1) {
+                    System.out.print("[Issue] Technician " + i + " is assigned to " + check2 + " teams on day " + j + ".\n");
                     feasible = 0;
                     //break; // if the technician is not assigned there is no point in carrying out the following checks
                 }
@@ -56,51 +50,55 @@ public class Checker {
         }
 
         // Check if the total duration of the e team's interventions on day d is less than 120 /////////////////////////////////////
-        for (int d = 1; d <= nb_days; d++) { // browsing the list of days
-            for (int e = 0; e < solution.getTech_teams()[d].getTeam().length; e++) { // browsing the list of teams
+        for (int d = 1; d < nb_days; d++) { // browsing the list of days
+            for (int e = 1; e < solution.getTech_teams()[d].getTeam().length; e++) { // browsing the list of teams
                 int cpt = 0; // to count the total working time of a day
-                for (int i = 0; i < nb_interv; i++) { // browsing the list of interventions
+                for (int i = 0; i < solution.getInterv_dates().length; i++) { // browsing the list of interventions
                     if (solution.getInterv_dates()[i].getDay() == d && solution.getInterv_dates()[i].getTeam() == e) {
-                        int ST = solution.getInterv_dates()[i].getInterv(); //
+                        int ST = solution.getInterv_dates()[i].getInterv(); //s
                         cpt = cpt + data.getIntervention()[ST].getTime();
                     }
                 }
                 if (cpt > 120) {
-                    System.out.print("[Issue] The total duration of the " + e + " team's interventions on day " + d + "is too long (>120).\n");
+                    System.out.print("[Issue] The total duration of the " + e + " team's interventions on day " + d + " is too long (>120).\n");
                     feasible = 0;
                 }
             }
         }
 
         //////////////////////////
-        for (int i = 0; i < nb_interv; i++) {
+        for (int i = 1; i < nb_interv; i++) {
             if (solution.getInterv_dates()[i].getInterv() != i + 1) {
                 break; //the outsourced interventions are not in Interv_dates, therefore avoids a shift with interv_list
             }
-            int start_i = solution.getInterv_dates()[i].getDay() * 120 + solution.getInterv_dates()[i].getTime();
+            int start_day = solution.getInterv_dates()[i].getTime();
             int ST = solution.getInterv_dates()[i].getInterv();
-            int end_i = start_i + data.getIntervention()[ST].getTime();
+            int end_i = start_day + data.getIntervention()[ST].getTime();
 
             // An intervention that has been started must be finished on the same day.
             if (end_i > 120) {
-                System.out.print("[Issue] Intervention" + i + " begin to late in day " + solution.getInterv_dates()[i].getDay() + " to be finish the same day.\n");
+                System.out.print("[Issue] Intervention " + i + " begin to late in day " + solution.getInterv_dates()[i].getDay() + " to be finish the same day.\n");
                 feasible = 0;
             }
 
             //  The intervention p belonging to Pred(i) ends before i begins ///
-            for (int p = 0; p < data.getIntervention()[i + 1].getPreds().length; p++) {
-                int pred = data.getIntervention()[i + 1].getPreds()[p];
-                boolean stop = true;
-                for (int l = 0; stop && i < solution.getInterv_dates().length; i++) {
-                    if (solution.getInterv_dates()[i].getInterv() == pred) {
-                        pred = l;
-                        stop = false;
-                    }
+            int start_i=0;
+            for (int l = 0; l < solution.getInterv_dates().length; l++) {
+                if (solution.getInterv_dates()[l].getInterv() == i) {
+                    start_i = solution.getInterv_dates()[l].getDay() * 120 + solution.getInterv_dates()[l].getTime();
                 }
-                int start_pred = solution.getInterv_dates()[pred].getDay() * 120 + solution.getInterv_dates()[pred].getTime();
-                if (data.getIntervention()[pred].getTime() + start_pred > start_i) {
-                    System.out.print("[Issue] Intervention " + pred + " is an intervention that precedes " + i + " and ends after i is started.\n");
-                    feasible = 0;
+            }
+            for (int p = 0; p < data.getIntervention()[i].getPreds().length; p++) {
+                int pred = data.getIntervention()[i].getPreds()[p];
+                for (int l = 0; l < solution.getInterv_dates().length; l++) {
+                    if (solution.getInterv_dates()[l].getInterv() == pred) {
+                        int start_pred = solution.getInterv_dates()[l].getDay() * 120 + solution.getInterv_dates()[l].getTime();
+                        if (data.getIntervention()[pred].getTime() + start_pred > start_i) {
+                            System.out.print("[Issue] Intervention " + pred + " is an intervention that precedes " + i + " and ends after " + i + " started.\n");
+                            feasible = 0;
+                        }
+
+                    }
                 }
             }
         }
@@ -109,18 +107,21 @@ public class Checker {
         int nb_ST = data.getIntervention().length - solution.getInterv_dates().length;
         int[] interv_ST = new int[nb_ST];
         int cpt = 0;
+        int id=0;
         for (int i = 0; i < nb_interv; i++) { //creation of a table of outsourced interventions
             if (solution.getInterv_dates()[cpt].getInterv() != data.getIntervention()[i + 1].getNumber()) {
-                interv_ST[i] = data.getIntervention()[i + 1].getNumber();
+                interv_ST[id] = data.getIntervention()[i + 1].getNumber();
+                id++;
             } else {
                 cpt++;
             }
         }
-
         //////we check if cost is below or equal to the budget
         int cost = 0;
         for (int value : interv_ST) {
-            cost = cost + data.getIntervention()[value].getCost(); //cost total of outsourced interventions
+            if(value != 0) {
+                cost = cost + data.getIntervention()[value + 1].getCost(); //cost total of outsourced interventions
+            }
         }
         if (cost > data.getInstance().getAbandon()) {
             System.out.print("[Issue] There is to many outsourced interventions, the total cost is higher than the budget.\n");
@@ -136,7 +137,7 @@ public class Checker {
                 }
                 if (check(data.getIntervention()[i].getPreds(), ST)) { //check if outsourced intervention j belongs to pred(i)
                     if (!check(interv_ST, data.getIntervention()[i].getNumber())) { //check if intervention i is outsourced
-                        System.out.print("[Issue] the intervention " + i + "is not outsourced while his predecessor " + ST + " is outsourced.\n");
+                        System.out.print("[Issue] the intervention " + i + " is not outsourced while his predecessor " + ST + " is outsourced.\n");
                         feasible = 0;
                     }
                 }
@@ -146,10 +147,15 @@ public class Checker {
         //////////we check that the team carrying out the intervention i has the skills in the different domains
         for (int i = 0; i < solution.getInterv_dates().length; i++) {
             int[][] dom_interv = new int[nb_domain][nb_level]; // array of domains and competences of intervention i
-            int[][] dom_team = new int[nb_domain][nb_level]; // aray of domains and competences of team that makes i
+            int[][] dom_team = new int[nb_domain][nb_level]; // array of domains and competences of team that makes i
             int num = solution.getInterv_dates()[i].getInterv();
             int day = solution.getInterv_dates()[i].getDay();
             int team = solution.getInterv_dates()[i].getTeam();
+            if(team>=solution.getTech_teams()[day].getTeam().length){
+                System.out.println("[Issue] Team number " + team + " on day " + day + " doesn't exist and is assigned to intervention " + i);
+                feasible = 0;
+               continue;
+            }
             int cpt1 = 0;
             //We fill in the array of domains and competences of intervention i
             for (int j = 0; j < nb_domain; j++) {
@@ -159,13 +165,14 @@ public class Checker {
                     cpt1++;
                 }
             }
+
             //We fill in the array of domains and competences of team that makes i
-            for (int m = 0; m < solution.getTech_teams()[day].getTeam()[team][0]; m++) {
+            for (int m = 0; m < solution.getTech_teams()[day].getTeam()[team].length; m++) {
                 int tech = solution.getTech_teams()[day].getTeam()[team][m];
                 for (int j = 0; j < nb_domain; j++) {
-                    int level = data.getTechnician()[tech].getD()[j];
+                    int level = data.getTechnician()[tech].getD()[j+1];
                     for (int k = 0; k < level; k++) {
-                        dom_team[j][k] = dom_team[j][k] + 1;
+                        dom_team[j][k] += 1;
                     }
                 }
             }
@@ -173,7 +180,7 @@ public class Checker {
             for (int j = 0; j < nb_domain; j++) {
                 for (int k = 0; k < nb_level; k++) {
                     if (dom_interv[j][k] > dom_team[j][k]) {
-                        System.out.print("[Issue] The team " + team + " of the day " + day + " is not competent enough in the domain " + j + "to do the intervention " + num + ".\n");
+                        System.out.print("[Issue] The team " + team + " of the day " + day + " is not competent enough in the domain " + j + " to do the intervention " + num + ".\n");
                         feasible = 0;
                     }
                 }
@@ -198,14 +205,15 @@ public class Checker {
         return b;
     }
 
-//    public static void main(String[] args) {
-//        //TTSPSolution ttspSolution = SolutionReader("./solutions/sol_A_3_#2");
-//        //TTSPData ttspsolution = InstanceReader("./solutions/sol_A_3_#2");
-//        System.out.print("----------------------------\n");
-//        System.out.print("----- CHECK CONSTRAINTS ----\n");
-//        System.out.print("----------------------------\n");
-//        System.out.print("----------------------------\n");
-//        //int result = checker(ttspData, ttspSolution);
-//        //System.out.print("-> FEASIBLE =" + result + "(0=false/1=true)\n");
-//    }
+    public static void main(String[] args) {
+        TTSPSolution ttspSolution = SolutionReader.parse("./datas/solutions/sol_A_2_#2");
+        TTSPData ttspData = InstanceReader.parse("./datas/solutions/sol_A_2_#2");
+        System.out.print("----------------------------\n");
+        System.out.print("----- CHECK CONSTRAINTS ----\n");
+        System.out.print("----------------------------\n");
+        System.out.print("----------------------------\n");
+        int result = checker(ttspData, ttspSolution);
+        System.out.println();
+        System.out.print("-> FEASIBLE = " + result + " (0=false/1=true)\n");
+    }
 }
