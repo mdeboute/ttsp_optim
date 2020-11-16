c4716c69abab8172934391c1c81c118cafd701d1
pbrebinaud <paul.brebinaud@gmail.com>
2020-11-16 16:59:10 +0100

    pbrebinaud : finition de Evaluator

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -13,9 +13,6 @@ public class Evaluator {
         int sum3;
 
         //Latest completion time for intervention of priority {1,2,3}
-        int LCT1 = 0;
-        int LCT2 = 0;
-        int LCT3 = 0;
         int C1 = 0;
         int C2 = 0;
         int C3 = 0;
@@ -25,15 +22,12 @@ public class Evaluator {
             int num = ttspsolution.getInterv_dates()[i].getInterv();
             if (ttspdata.getIntervention()[num].getPrio() == 1 && C1 < (120 * (ttspsolution.getInterv_dates()[i].getDay()-1) + ttspsolution.getInterv_dates()[i].getTime())) {
                 C1 = 120 * (ttspsolution.getInterv_dates()[i].getDay()-1) + ttspsolution.getInterv_dates()[i].getTime();
-                LCT1 = ttspsolution.getInterv_dates()[i].getTime();
             }
             if (ttspdata.getIntervention()[num].getPrio() == 2 && C2 < (120 * (ttspsolution.getInterv_dates()[i].getDay()-1) + ttspsolution.getInterv_dates()[i].getTime())) {
                 C2 = 120 * (ttspsolution.getInterv_dates()[i].getDay()-1) + ttspsolution.getInterv_dates()[i].getTime();
-                LCT2 = ttspsolution.getInterv_dates()[i].getTime();
             }
             if (ttspdata.getIntervention()[num].getPrio() == 3 && C3 < (120 * (ttspsolution.getInterv_dates()[i].getDay()-1) + ttspsolution.getInterv_dates()[i].getTime())) {
                 C3 = 120 * (ttspsolution.getInterv_dates()[i].getDay()-1) + ttspsolution.getInterv_dates()[i].getTime();
-                LCT3 = ttspsolution.getInterv_dates()[i].getTime();
             }
         }
 
@@ -41,15 +35,15 @@ public class Evaluator {
         sum1 = 28 * C1;
         sum2 = 14 * C2;
         sum3 = 4 * C3;
-        int Scost = LCT1;
-        if (LCT2 > Scost){
-            Scost = LCT2;
+        int Scost = C1;
+        if (C2 > Scost){
+            Scost = C2;
         }
-        if (LCT3 > Scost){
-            Scost = LCT3;
+        if (C3 > Scost){
+            Scost = C3;
         }
         int somme = sum1 + sum2 + sum3 + Scost;
-        return new int[]{sum1, LCT1, sum2, LCT2, sum3, LCT3, Scost, somme};
+        return new int[]{sum1, C1, sum2, C2, sum3, C3, Scost, somme};
     }
 
    public static void main(String[] args) {
